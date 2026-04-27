22af90cd9f3bc1657a9daaae073145dfff5ef790
Ricki Friadi <ricki.vendor.pasti@pegadaian.co.id>
2026-04-27 12:53:53 +0700

    feat: add left-rail and bottom-tab content offset to MainLayout

---
+++ src/components/layout/MainLayout.vue
@@ -0,0 +1,19 @@
+<script setup lang="ts">
+import { computed } from 'vue'
+import { useRoute } from 'vue-router'
+import AppFooter from './AppFooter.vue'
+import AppHeader from './AppHeader.vue'
+
+const route = useRoute()
+const isImmersiveRoute = computed(() => route.path === '/works')
+</script>
+
+<template>
+  <div class="relative min-h-screen overflow-x-clip">
+    <AppHeader v-if="!isImmersiveRoute" />
+    <main class="relative flex-1" :class="!isImmersiveRoute ? 'pb-16 lg:pb-0 lg:pl-[220px]' : ''">
+      <slot />
+    </main>
+    <AppFooter v-if="!isImmersiveRoute" />
+  </div>
+</template>
