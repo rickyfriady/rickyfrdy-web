205664971c52324bfff1f12db626c0fd6f809ede
Ricki Friadi <ricki.vendor.pasti@pegadaian.co.id>
2026-04-28 16:24:07 +0700

    feat: centered 680px column with diagonal hatch gutters

--- src/components/layout/MainLayout.vue
+++ src/components/layout/MainLayout.vue
@@ -9,11 +9,26 @@ const isImmersiveRoute = computed(() => route.path === '/works')
 </script>
 
 <template>
-  <div class="relative min-h-screen overflow-x-clip">
-    <AppHeader v-if="!isImmersiveRoute" />
-    <main class="relative flex-1" :class="!isImmersiveRoute ? 'pb-28 lg:pb-0 lg:pl-[220px]' : ''">
+  <!-- Immersive route (/works): full-screen, no chrome -->
+  <template v-if="isImmersiveRoute">
+    <main class="relative min-h-screen">
       <slot />
     </main>
-    <AppFooter v-if="!isImmersiveRoute" />
-  </div>
+  </template>
+
+  <!-- Normal routes: hatch bg + centered 680px column -->
+  <template v-else>
+    <AppHeader />
+
+    <!-- Outer wrapper carries the hatch texture as the page background -->
+    <div class="hatch-gutter relative min-h-screen">
+      <!-- Centered column: white bg, left+right border, sits on the hatch -->
+      <div class="border-border bg-background mx-auto min-h-screen max-w-[680px] border-x pt-14">
+        <main class="pb-28 sm:pb-8">
+          <slot />
+        </main>
+        <AppFooter />
+      </div>
+    </div>
+  </template>
 </template>
