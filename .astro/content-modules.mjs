fc2221df4a86c8d321edf8347206a4a8bf19f7a9
ricki friadi <friadi.ricki@gmail.com>
2026-05-16 11:17:52 +0700

    chore: commit astro generated types, restore pre-commit hook, add case study plan doc

---
+++ .astro/content-modules.mjs
@@ -0,0 +1,5 @@
+
+export default new Map([
+["src/content/blog/microservices-with-nestjs.mdx", () => import("astro:content-layer-deferred-module?astro%3Acontent-layer-deferred-module=&fileName=src%2Fcontent%2Fblog%2Fmicroservices-with-nestjs.mdx&astroContentModuleFlag=true")],
+["src/content/projects/singel-app.mdx", () => import("astro:content-layer-deferred-module?astro%3Acontent-layer-deferred-module=&fileName=src%2Fcontent%2Fprojects%2Fsingel-app.mdx&astroContentModuleFlag=true")]]);
+		
